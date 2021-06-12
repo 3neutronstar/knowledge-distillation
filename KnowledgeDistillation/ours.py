fix the covariance loss and add the selfkd learner

KnowledgeDistillation/ours.py
@@ -12,26 +12,30 @@ class PearsonCorrelationLoss(nn.Module):
         kd_loss=self._covariance_loss(output,target)
         return kd_loss
 
-    def _covariance_loss(self,logits, labels):
-        bsz, n_cats, n_heads = logits.size()
-        if n_heads < 2:
-            return 0
-        all_probs = torch.softmax(logits/self.T, dim=1)
-        label_inds = torch.ones(bsz, n_cats).cuda()
-        label_inds[range(bsz), labels] = 0
-        # removing the ground truth prob
-        probs = all_probs * label_inds.unsqueeze(-1).detach()
-        # re-normalize such that probs sum to 1
-        #probs /= (all_probs.sum(dim=1, keepdim=True) + 1e-8)
-        probs = (torch.softmax(logits/self.T, dim=1) + 1e-8)
-        # cosine regularization
-        #### I added under 2-line
-        probs -= probs.mean(dim=1, keepdim=True)
-        probs = probs / torch.sqrt(((probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
-        ####
-        #probs = probs / torch.sqrt(((all_probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
-        cov_mat = torch.einsum('ijk,ijl->ikl', probs, probs)
-        pairwise_inds = 1 - torch.eye(n_heads).cuda()
-        den = bsz * (n_heads -1) * n_heads
-        loss = ((cov_mat * pairwise_inds).abs().sum() / den)
+    def _covariance_loss(self, logits, labels):
+        loss=0.0
+        for label in labels.unique():
+            logits_label=logits[labels==label]
+            labels_label=labels[labels==label].clone()
+            batch_size, n_cats= logits_label.size()
+            # removing the ground truth prob
+            all_probs = F.softmax(logits_label/self.T, dim=1)
+
+            label_inds=torch.ones_like(logits_label)
+            label_inds[range(batch_size),labels_label.long()] = 0
+            probs = all_probs * label_inds.detach()
+
+            # re-normalize such that probs sum to 1
+            #probs /= (all_probs.sum(dim=1, keepdim=True) + 1e-8)
+            probs = (F.softmax(logits_label/self.T, dim=1) + 1e-8)
+            
+            # cosine regularization
+            #### I added under 2-line
+            probs -= probs.mean(dim=1, keepdim=True)
+            probs = probs / torch.sqrt(((probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
+            ####
+            #probs = probs / torch.sqrt(((all_probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
+            cov_mat = torch.mm(probs,probs.T)
+            cov_mat[torch.eye(batch_size,dtype=torch.bool,device='cuda')]=0.0
+            loss -= cov_mat.norm(p=1) / batch_size
         return loss

Learner/selfkd_learner.py
@@ -5,7 +5,7 @@ from KnowledgeDistillation.baseKD import *
 import time
 import logging
 CUSTOM_LOSS={
-    'softtarget':OFFKD['softtarget'],
+    'cs-kd':OFFKD['softtarget'],
     'baseline':None,
     'pearson':OURS['ver1'],
 }
@@ -54,26 +54,35 @@ class SelfKDLearner(ClassicLearner):
             inputs, targets = inputs.to(self.device), targets.to(self.device)
 
             batch_size = inputs.size(0)
-
             if not self.KDCustomLoss:
                 outputs = self.model(inputs)
-                loss = torch.mean(self.criterion(outputs, targets))
+                loss = self.criterion(outputs, targets)
                 train_loss += loss.item()
 
                 _, predicted = torch.max(outputs, 1)
                 total += targets.size(0)
                 correct += predicted.eq(targets.data).sum().float().cpu()
             else:
-                targets_ = targets[:batch_size//2]
-                outputs = self.model(inputs[:batch_size//2])
-                loss = torch.mean(self.criterion(outputs, targets_))
-                train_loss += loss.item()
+                if self.configs['custom_loss']=='cs-kd':
+                    targets_ = targets[:batch_size//2].detach().clone()
+                    outputs = self.model(inputs[:batch_size//2])
+                    loss = self.criterion(outputs, targets_)
+                    train_loss += loss.item()
 
-                with torch.no_grad():
-                    outputs_cls = self.model(inputs[batch_size//2:])
-                cls_loss = self.kdloss(outputs, outputs_cls.detach())
-                loss += self.configs['lambda'] * cls_loss
-                train_cls_loss += cls_loss.item()
+                    with torch.no_grad():
+                        outputs_cls = self.model(inputs[batch_size//2:])
+                    cls_loss = self.kdloss(outputs, outputs_cls.detach())
+                    loss += self.configs['lambda'] * cls_loss
+                    train_cls_loss += cls_loss.item()
+                else:
+                    targets_=targets
+                    outputs=self.model(inputs)
+                    loss = self.criterion(outputs, targets_)
+                    train_loss += loss.item()
+
+                    cls_loss=self.kdloss(outputs,targets_)
+                    loss += self.configs['lambda'] * cls_loss
+                    train_cls_loss += cls_loss.item()
 
                 _, predicted = torch.max(outputs, 1)
                 total += targets_.size(0)
@@ -84,7 +93,7 @@ class SelfKDLearner(ClassicLearner):
             self.optimizer.step()
             if batch_idx % self.log_interval == 0:
                 print('\r Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, total
-                , len(self.train_loader.dataset)//2, 100.0 * total / len(self.train_loader.dataset), train_loss/(batch_idx+1)), end='')
+                , len(self.train_loader.dataset), 100.0 * total / len(self.train_loader.dataset), train_loss/(batch_idx+1)), end='')
         tok=time.time()
         running_accuracy=100.0*float(correct)/float(total)
 

data_load.py
@@ -2,8 +2,9 @@ from torchvision import datasets
 import torchvision.transforms as transforms
 import torch
 import sys
-from six.moves import urllib
-
+from six.moves import urllib 
+from torch.utils.data import BatchSampler, SequentialSampler, RandomSampler, Subset
+from utils import PairBatchSampler
 def load_dataset(configs):
     if sys.platform == 'linux':
         data_save_path='dataset'
@@ -76,6 +77,20 @@ def load_dataset(configs):
         train_data=datasets.ImageNet(root=data_save_path,train=True,transform=data_transforms['train'],download=True )
         test_data=datasets.ImageNet(root=data_save_path,train=False,transform=data_transforms['test'],download=False )
 
+    elif configs['dataset']=='fashionmnist':
+        train_transform=transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        test_transform=transforms.Compose([
+            transforms.Resize((32, 32)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,))
+        ])
+        train_data=datasets.FashionMNIST(root=data_save_path, download=True, train=True, transform=train_transform)
+        test_data=datasets.FashionMNIST(root=data_save_path, download=False, train=False, transform=test_transform)
+    
     return train_data, test_data
 
 def base_data_loader(train_data,test_data,configs):
@@ -100,12 +115,43 @@ def base_data_loader(train_data,test_data,configs):
     print("Using Datasets: ", configs['dataset'])
     return train_data_loader, test_data_loader
 
+def pair_data_loader(train_data,test_data,configs):
+    if configs['device'] == 'gpu':
+        pin_memory = True
+        # pin_memory=False
+    else:
+        pin_memory = False
+        # Sampler
+    if configs['sample'] == 'default':
+        get_train_sampler = lambda d: BatchSampler(RandomSampler(d), configs['batch_size'], False)
+        get_test_sampler  = lambda d: BatchSampler(SequentialSampler(d), configs['batch_size'], False)
+
+    elif configs['sample'] == 'pair':
+        get_train_sampler = lambda d: PairBatchSampler(d, configs['batch_size'])
+        get_test_sampler  = lambda d: BatchSampler(SequentialSampler(d), configs['batch_size'], False)
+
+    train_data_loader = torch.utils.data.DataLoader(train_data,
+                                                    batch_sampler=get_train_sampler(train_data),
+                                                    pin_memory=pin_memory,
+                                                    num_workers=configs['num_workers'],
+                                                    )
+    test_data_loader = torch.utils.data.DataLoader(test_data,
+                                                    batch_sampler=get_test_sampler(test_data),
+                                                    pin_memory=pin_memory,
+                                                    num_workers=configs['num_workers'],
+                                                    )
+
+    return train_data_loader, test_data_loader
 def data_loader(configs):
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/5.0')]
     urllib.request.install_opener(opener)
     train_data, test_data = load_dataset(configs)
-    if configs['mode']=='train' or configs['mode']=='test' or 'kd' in configs['mode']:
-        train_data_loader, test_data_loader=base_data_loader(train_data, test_data,configs)
 
+
+    if 'self' in configs['mode']:
+        train_data_loader,test_data_loader=pair_data_loader(train_data,test_data,configs)
+    elif configs['mode']=='train' or configs['mode']=='test' or 'kd' in configs['mode']:
+        train_data_loader, test_data_loader=base_data_loader(train_data, test_data,configs)
+    
     return train_data_loader, test_data_loader