74b308c2121e54b436dbbb44acf05d9480b1530d
3neutronstar <minsoo.kang0918@gmail.com>
2021-06-14 11:32:06 +0900

    add the nesterov option and weight decay option

--- KnowledgeDistillation/ours.py
+++ KnowledgeDistillation/ours.py
@@ -12,32 +12,31 @@ class PearsonCorrelationLoss(nn.Module):
         kd_loss=self._covariance_loss(output,target)
         return kd_loss
 
-    # def _covariance_loss(self, logits, labels):
-    def _covariance_loss(self, logits_label, labels_label):
+    def _covariance_loss(self, logits, labels):
         loss=0.0
-        # for label in labels.unique():
-            # logits_label=logits[labels==label]
-            # labels_label=labels[labels==label].clone()
-        batch_size, n_cats= logits_label.size()
-        # removing the ground truth prob
-        all_probs = F.softmax(logits_label/self.T, dim=1)
+        for label in labels.unique():
+            logits_label=logits[labels==label]
+            labels_label=labels[labels==label].clone()
+            batch_size, n_cats= logits_label.size()
+            # removing the ground truth prob
+            all_probs = F.softmax(logits_label/self.T, dim=1)
 
-        label_inds=torch.ones_like(logits_label)
-        label_inds[range(batch_size),labels_label.long()] = 0
-        probs = all_probs * label_inds.detach()
+            label_inds=torch.ones_like(logits_label)
+            label_inds[range(batch_size),labels_label.long()] = 0
+            probs = all_probs * label_inds.detach()
 
-        # re-normalize such that probs sum to 1
-        #probs /= (all_probs.sum(dim=1, keepdim=True) + 1e-8)
-        probs = (F.softmax(logits_label/self.T, dim=1) + 1e-8)
-        
-        # cosine regularization
-        #### I added under 2-line
-        probs -= probs.mean(dim=1, keepdim=True)
-        probs = probs / torch.sqrt(((probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
-        ####
-        #probs = probs / torch.sqrt(((all_probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
-        cov_mat = torch.mm(probs,probs.T)
-        cov_mat[torch.eye(batch_size,dtype=torch.bool,device='cuda')]=0.0
-        loss -= cov_mat.norm(p=1) / batch_size
-        # loss/=labels.unique().size()[0]
+            # re-normalize such that probs sum to 1
+            #probs /= (all_probs.sum(dim=1, keepdim=True) + 1e-8)
+            probs = (F.softmax(logits_label/self.T, dim=1) + 1e-8)
+            
+            # cosine regularization
+            #### I added under 2-line
+            probs -= probs.mean(dim=1, keepdim=True)
+            probs = probs / torch.sqrt(((probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
+            ####
+            #probs = probs / torch.sqrt(((all_probs ** 2).sum(dim=1, keepdim=True) + 1e-8))
+            cov_mat = torch.mm(probs,probs.T)
+            cov_mat[torch.eye(batch_size,dtype=torch.bool,device='cuda')]=0.0
+            loss -= cov_mat.norm(p=1) / batch_size
+        loss/=labels.unique().size()[0]
         return loss
--- Model/lenet5.py
+++ Model/lenet5.py
@@ -12,39 +12,45 @@ class LeNet5(nn.Module):
         self.subsampling = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
         self.conv2 = nn.Conv2d(
             in_channels=6, out_channels=16, kernel_size=(5, 5))  # 5x5+1 params
-        self.conv3 = nn.Conv2d(
-            in_channels=16, out_channels=120, kernel_size=(5, 5))  # 5x5+1 params
-        self.fc1 = nn.Linear(120, 84)
-        self.fc2 = nn.Linear(84, configs['num_classes'])
+        self.fc1 = nn.Linear(400, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, configs['num_classes'])
         
-        #self.log_softmax = nn.LogSoftmax(dim=-1)
         self.optim = optim.SGD(params=self.parameters(),
                                momentum=configs['momentum'], lr=configs['lr'], nesterov=configs['nesterov'], weight_decay=configs['weight_decay'])
         self.loss=nn.CrossEntropyLoss()
-        # self.scheduler=optim.lr_scheduler.StepLR(self.optim,step_size=15,gamma=0.1)
-        self.scheduler=optim.lr_scheduler.ExponentialLR(self.optim,gamma=0.98)
+        self.scheduler=optim.lr_scheduler.StepLR(self.optim,step_size=15,gamma=0.1)
+        # self.scheduler=optim.lr_scheduler.ExponentialLR(self.optim,gamma=0.98)
+        self.w_size_list = [150, 2400, 48000, 10080, 84*configs['num_classes']]  # weight,bias size
+        self.b_size_list = [6, 16, 120, 84, configs['num_classes']]
+        self.NN_size_list = [1, 6, 16, 120, 84, configs['num_classes']]  # cnn과 fc_net out 작성
+        self.model_list = ['cnn', 'cnn', 'fc', 'fc', 'fc']
+        self.kernel_size_list = [(5, 5), (5, 5)]
+        self.node_size_list=[6,16,120,84,configs['num_classes']]
+
     def forward(self, x):
-        x = F.relu(self.conv1(x),inplace=True)
+        x = F.relu(self.conv1(x))
         x = self.subsampling(x)
-        x = F.relu(self.conv2(x),inplace=True)
+        x = F.relu(self.conv2(x))
         x = self.subsampling(x)
-        x = F.relu(self.conv3(x),inplace=True)
-        x = x.view(-1, 120)
-        x = F.relu(self.fc1(x),inplace=True)
+        x = x.view(-1, 400)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
         # print(self.fc1.weight.size())
         # print(torch.nonzero(self.fc1.weight).size(),'weight')
-        x = self.fc2(x)
-        #x = self.log_softmax(x)
         return x
     
     def extract_feature(self,x):
-        x = F.relu(self.conv1(x),inplace=True)
+        x = F.relu(self.conv1(x))
         x = self.subsampling(x)
-        x = F.relu(self.conv2(x),inplace=True)
-        x = self.subsampling(x)
-        feature = F.relu(self.conv3(x),inplace=True)
-        x = feature.view(-1, 120)
-        x = F.relu(self.fc1(x),inplace=True)
-        x = self.fc2(x)
-        #x = self.log_softmax(x)
-        return x,feature
+        x = F.relu(self.conv2(x))
+        feature = self.subsampling(x)
+        x = feature.view(-1, 400)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x, feature
+
+    def get_configs(self):
+        return self.w_size_list,self.b_size_list,self.NN_size_list,self.model_list,self.kernel_size_list,self.node_size_list
